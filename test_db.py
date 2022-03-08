85a300f66c7e8e12a109620d0407f2d080553b2c
ms <vomircom@gmail.com>
2022-03-08 22:36:15 +0100

    Update test_db.py

--- test_db.py
+++ test_db.py
@@ -1,12 +1,11 @@
 import mysql.connector
-con = mysql.connector.connect(host='127.0.0.1',
-                                         user='<enter user, root',
-                                         database='<enter database, mysql',
-                                         password='<enter password')
+import json
+
+con = mysql.connector.connect(option_files='my.conf')
 
 sqlite_table = """ CREATE TABLE note (
     id int NOT NULL AUTO_INCREMENT,
-    content varchar(20) NOT NULL,
+    content varchar(255) NOT NULL,
     PRIMARY KEY(id)
 )"""
 #create db
