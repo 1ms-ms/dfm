3ca845d25bd3e8ebcc2b3b74d6d0cdcdcc6b2264
ms <vomircom@gmail.com>
2022-03-08 17:59:12 +0100

    Update test_db.py

--- test_db.py
+++ test_db.py
@@ -1,12 +1,13 @@
 import mysql.connector
 con = mysql.connector.connect(host='127.0.0.1',
-                                         user='root',
-                                         database='mysql',
-                                         password='PASS')
+                                         user='<enter user, root',
+                                         database='<enter database, mysql',
+                                         password='<enter password')
 
 sqlite_table = """ CREATE TABLE note (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    content VARCHAR(255) NOT NULL
+    id int NOT NULL AUTO_INCREMENT,
+    content varchar(20) NOT NULL,
+    PRIMARY KEY(id)
 )"""
 #create db
 cur = con.cursor()
