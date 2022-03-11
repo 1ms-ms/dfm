0cb9aeeef8a1e21ee776dfdc5e8a184dec54d9de
ms <vomircom@gmail.com>
2022-03-11 16:03:23 +0100

    Update config.sql

--- config.sql
+++ config.sql
@@ -2,11 +2,3 @@ CREATE TABLE note (
     id int NOT NULL AUTO_INCREMENT,
     content varchar(255) NOT NULL,
     PRIMARY KEY(id));
-
-GRANT ALL PRIVILEGES  ON testdb.* TO 'testuser'@'localhost' IDENTIFIED BY 'testpass' WITH GRANT OPTION;
-
-GRANT ALL PRIVILEGES ON *.* TO 'testuser'@'localhost' WITH GRANT OPTION;
-
-GRANT ALL PRIVILEGES  ON testdb.* TO 'testuser'@'%' IDENTIFIED BY 'testpass' WITH GRANT OPTION;
-
-GRANT ALL PRIVILEGES ON *.* TO 'testuser'@'%' WITH GRANT OPTION;
